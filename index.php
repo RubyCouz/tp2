52bc5830a0bc78274719a186a014d55332029072
rubycouzcedric <cousin.ruby.cedric@gmail.com>
2018-08-23 08:53:52 +0200

    ajout de css

--- index.php
+++ index.php
@@ -10,114 +10,10 @@
 
     </head>
     <body>
-        <div class="container">
+        <div class="container-fluid">
             <div class="row justify-content-around">
                 <div class="col-sm-12-md-12-lg-6">
-                    <?php
-                    if (isset($_POST['submit'])) { // si des données sont envoyés
-                        ?>
-                        <h2>Vos informations :</h2>                    
-                        <p>Civilité : 
-                            <?php
-                            if (!empty($_POST['civility'])) { //si le champs de saisie contient des informations
-                                ?>
-                                <span class="valid">
-                                    <?php
-                                    echo $_POST['civility']; // affichage des données
-                                    ?>
-                                </span>
-                                <?php
-                            } else {
-                                ?>
-                                <span class="error">
-                                    <?php
-                                    echo 'Veuillez saisir votre civilité'; //message d'erreur en cas d'absence d'info
-                                }
-                                ?>
-                            </span>
-                        </p>
-                        <p>Nom : 
-                            <?php
-                            if (!empty($_POST['lastname'])) { //si le champs de saisie contient des informations
-                                ?>
-                                <span class="valid">
-                                    <?php
-                                    echo $_POST['lastname']; // affichage des données
-                                    ?>
-                                </span>
-                                <?php
-                            } else {
-                                ?>
-                                <span class="error">
-                                    <?php
-                                    echo 'Veuillez saisir votre nom'; //message d'erreur en cas d'absence d'info
-                                }
-                                ?>
-                            </span>
-                        </p>
-                        <p>Prénom : 
-                            <?php
-                            if (!empty($_POST['firstname'])) { //si le champs de saisie contient des informations
-                                ?>
-                                <span class="valid">
-                                    <?php
-                                    echo $_POST['firstname']; // affichage des données
-                                    ?>
-                                </span>
-                                <?php
-                            } else {
-                                ?>
-                                <span class="error">
-                                    <?php
-                                    echo 'Veuillez saisir votre prénom'; //message d'erreur en cas d'absence d'info
-                                }
-                                ?>
-                            </span>
-                        </p>
-                        <p>Age : 
-                            <?php
-                            if (!empty($_POST['age'])) { //si le champs de saisie contient des informations
-                                ?>
-                                <span class="valid">
-                                    <?php
-                                    echo $_POST['age']; // affichage des données
-                                    ?>
-                                </span>
-                                <?php
-                            } else {
-                                ?>
-                                <span class="error">
-                                    <?php
-                                    echo 'Veuillez saisir votre Age'; //message d'erreur en cas d'absence d'info
-                                }
-                                ?>
-                            </span>
-                        </p>
-                        <p>Sociéte : 
-                            <?php
-                            if (!empty($_POST['enterprise'])) { //si le champs de saisie contient des informations
-                                ?>
-                                <span class="valid">
-                                    <?php
-                                    echo $_POST['enterprise']; // affichage des données
-                                    ?>
-                                </span>
-                                <?php
-                            } else {
-                                ?>
-                                <span class="error">
-                                    <?php
-                                    echo 'Veuillez saisir votre société'; //message d'erreur en cas d'absence d'info
-                                }
-                                ?>
-                            </span>
-                        </p>
-                        <?php
-                    }
-                    ?>
-                </div>
-                <div class="col-sm-12-md-12-lg-6">
-                    <h1>Demande de renseignements :</h1>
+                    <h2>Demande de renseignements :</h2>
                     <div class="row">
                         <div class="col-sm-12-md-12-lg-12">
                             <form action="index.php" method="POST">
@@ -144,9 +40,115 @@
                                     <label for="enterprise">Société</label>
                                     <input value="<?= (isset($_POST['enterprise']) ? $_POST['enterprise'] : '') ?>" name="enterprise" type="text" class="form-control" id="enterprise" aria-describedby="enterpriseHelp" placeholder="Société">
                                 </div>
-                                <button name="submit" type="submit" class="btn btn-primary">Submit</button>
-                            </form>                    
+                                <button name="submit" type="submit" class="btn">Afficher</button>
+                            </form>                
                         </div>
+                    </div>
+                </div>
+                <div class="col-sm-12-md-12-lg-6">
+                    <div class="info">
+                        <?php
+                        if (isset($_POST['submit'])) { // si des données sont envoyés
+                            ?>
+                            <h2>Vos informations :</h2>                    
+                            <p>Civilité : 
+                                <?php
+                                if (!empty($_POST['civility'])) { //si le champs de saisie contient des informations
+                                    ?>
+                                    <span class="valid">
+                                        <?php
+                                        echo $_POST['civility']; // affichage des données
+                                        ?>
+                                    </span>
+                                    <?php
+                                } else {
+                                    ?>
+                                    <span class="error">
+                                        <?php
+                                        echo 'Veuillez saisir votre civilité'; //message d'erreur en cas d'absence d'info
+                                    }
+                                    ?>
+                                </span>
+                            </p>
+                            <p>Nom : 
+                                <?php
+                                if (!empty($_POST['lastname'])) { //si le champs de saisie contient des informations
+                                    ?>
+                                    <span class="valid">
+                                        <?php
+                                        echo $_POST['lastname']; // affichage des données
+                                        ?>
+                                    </span>
+                                    <?php
+                                } else {
+                                    ?>
+                                    <span class="error">
+                                        <?php
+                                        echo 'Veuillez saisir votre nom'; //message d'erreur en cas d'absence d'info
+                                    }
+                                    ?>
+                                </span>
+                            </p>
+                            <p>Prénom : 
+                                <?php
+                                if (!empty($_POST['firstname'])) { //si le champs de saisie contient des informations
+                                    ?>
+                                    <span class="valid">
+                                        <?php
+                                        echo $_POST['firstname']; // affichage des données
+                                        ?>
+                                    </span>
+                                    <?php
+                                } else {
+                                    ?>
+                                    <span class="error">
+                                        <?php
+                                        echo 'Veuillez saisir votre prénom'; //message d'erreur en cas d'absence d'info
+                                    }
+                                    ?>
+                                </span>
+                            </p>
+                            <p>Age : 
+                                <?php
+                                if (!empty($_POST['age'])) { //si le champs de saisie contient des informations
+                                    ?>
+                                    <span class="valid">
+                                        <?php
+                                        echo $_POST['age']; // affichage des données
+                                        ?>
+                                    </span>
+                                    <?php
+                                } else {
+                                    ?>
+                                    <span class="error">
+                                        <?php
+                                        echo 'Veuillez saisir votre Age'; //message d'erreur en cas d'absence d'info
+                                    }
+                                    ?>
+                                </span>
+                            </p>
+                            <p>Sociéte : 
+                                <?php
+                                if (!empty($_POST['enterprise'])) { //si le champs de saisie contient des informations
+                                    ?>
+                                    <span class="valid">
+                                        <?php
+                                        echo $_POST['enterprise']; // affichage des données
+                                        ?>
+                                    </span>
+                                    <?php
+                                } else {
+                                    ?>
+                                    <span class="error">
+                                        <?php
+                                        echo 'Veuillez saisir votre société'; //message d'erreur en cas d'absence d'info
+                                    }
+                                    ?>
+                                </span>
+                            </p>
+                            <?php
+                        }
+                        ?>
                     </div>
                 </div>
             </div>
